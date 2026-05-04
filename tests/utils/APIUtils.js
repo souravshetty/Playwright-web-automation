66c50d17fc42f7794c180ef98d8355ba37243976
souravshetty <sourav.s.shetty@gmail.com>
2026-05-03 21:42:11 -0700

    feat:Utils file for api test

---
+++ tests/utils/APIUtils.js
@@ -0,0 +1,43 @@
+class APIUtils {
+	constructor(apiContext, loginPayLoad) {
+		this.apiContext = apiContext;
+		this.loginPayLoad = loginPayLoad;
+	}
+
+	async getToken() {
+		const loginResponse = await this.apiContext.post(
+			"https://rahulshettyacademy.com/api/ecom/auth/login",
+			{
+				data: this.loginPayLoad,
+			},
+		); // 200, 201
+		const loginResponseJson = await loginResponse.json();
+		const token = loginResponseJson.token;
+		console.log(token);
+		return token;
+	}
+
+	async createOrder(orderPayLoad) {
+		let response = {};
+		response.token = await this.getToken();
+		const orderResponse = await this.apiContext.post(
+			"https://rahulshettyacademy.com/client/#/dashboard/myorders",
+			{
+				data: orderPayLoad,
+				headers: {
+					Authorization: response.token,
+					"Content-Type": "application/json",
+				},
+			},
+		);
+
+		const orderResponseJson = await orderResponse.json();
+		console.log(orderResponseJson);
+		const orderId = orderResponseJson.orders[0];
+		response.orderId = orderId;
+
+		return response;
+	}
+}
+
+module.exports = { APIUtils };
