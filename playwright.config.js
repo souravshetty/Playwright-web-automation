6a4bd338df07c9f4311d178188a67efe94ac5239
souravshetty <sourav.s.shetty@gmail.com>
2026-05-05 21:39:53 -0700

    feat accesss the storage values to pass into script

--- playwright.config.js
+++ playwright.config.js
@@ -14,13 +14,16 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
 	testDir: "./tests",
-	timeout: 40 * 1000,
+	timeout: 100 * 1000,
 	expect: {
 		timeout: 40 * 1000,
 	},
+	reporter: "html",
 
 	use: {
 		browserName: "chromium",
-		// headless: true,
+		headless: false,
+		screenshot: "on",
+		trace: "on",
 	},
 });
